0de085e8289788e40ecfb241fda3a13e3c628c47
Grayson Connor <grayson.bolon@yahoo.com>
2021-09-09 05:58:08 -0500

    v1 complete

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ const gameResultContainer = document.querySelector("#gameResultContainer");
 const playerResult = document.querySelector("#playerResult");
 const computerResult = document.querySelector("#computerResult");
 const resultTitle = document.querySelector("#resultTitle");
+const resultBtns = document.querySelector("#resultBtns");
 const playAgain = document.querySelector("#playAgain");
 const newGame = document.querySelector("#newGame");
-const score = document.querySelector("#score");
+const playerScoreOp = document.querySelector("#playerScoreOp");
+const computerScoreOp = document.querySelector("#computerScoreOp");
 const rules = document.querySelector("#rules-btn");
 const closeRules = document.querySelector("#closeRules");
 const rulesModal = document.querySelector(".rules-modal");
+const bestOfThreeCheck = document.querySelector("#bestOfThree");
+const bestOfThreeOp = document.querySelector("#bestOfThreeOp");
+const scoreBox = document.querySelector(".score-box");
+const bestOfThreeCount = document.querySelector("#bestOfThreeCount");
 const options = [
 	{
 		name: "rock",
@@ -31,7 +37,9 @@ const options = [
 	}
 ];
 
-let scoreCount = 0;
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
 
 //function that links user button selection to corresponding object in options array
 gameBtns.forEach(btn => {
@@ -42,7 +50,11 @@ gameBtns.forEach(btn => {
 		const playerSelect = btn.dataset.selection;
 		//gets user picked, returns object from 'options' array
 		const option = options.find(option => option.name == playerSelect);
-		makeSelection(option);
+		if (bestOfThreeCheck.checked === true) {
+			bestOfThree(option);
+		} else {
+			makeSelection(option);
+		}
 	});
 });
 
@@ -60,26 +72,105 @@ closeRules.addEventListener("click", () => {
 });
 
 newGame.addEventListener("click", () => {
-	arena.style.display = "flex";
-	gameResultContainer.style.display = "none";
-	scoreCount = 0;
-	score.textContent = scoreCount;
+	startNewGame();
 });
 
+bestOfThreeCheck.addEventListener("change", () => {
+	if (bestOfThreeCheck.checked == true) {
+		bestOfThreeOp.style.display = "block";
+		scoreBox.classList.add("bestOfThreeOn");
+	} else {
+		bestOfThreeOp.style.display = "none";
+		scoreBox.classList.remove("bestOfThreeOn");
+	}
+	startNewGame();
+});
+
+function startNewGame() {
+	arena.style.display = "flex";
+	gameResultContainer.style.display = "none";
+	playerScore = 0;
+	computerScore = 0;
+	playerScoreOp.textContent = playerScore;
+	computerScoreOp.textContent = computerScore;
+	playerScoreOp.style.color = "black";
+	computerScoreOp.style.color = "black";
+	playAgain.style.display = "block";
+	newGame.style.display = "block";
+	playAgain.textContent = "play again";
+	newGame.textContent = "reset score";
+	resultBtns.style.justifyContent = "space-between";
+	round = 0;
+	bestOfThreeCount.textContent = `${round}`;
+	resultTitle.style.fontSize = "5rem";
+	resultTitle.style.color = "white";
+}
+
 function makeSelection(selection) {
-	const computerSelection = computerPick();
-	const youWin = isWinner(selection, computerSelection);
-	const youLose = isWinner(computerSelection, selection);
+	let computerSelection = computerPick();
+	let youWin = isWinner(selection, computerSelection);
+	let youLose = isWinner(computerSelection, selection);
 	resultStyle(selection, computerSelection);
 	if (youWin) {
 		resultTitle.textContent = "you win";
-		scoreCount++;
+		playerScore++;
 	} else if (youLose) {
 		resultTitle.textContent = "you lose";
+		computerScore++;
 	} else {
 		resultTitle.textContent = "tie";
 	}
-	score.textContent = scoreCount;
+	playerScoreOp.textContent = playerScore;
+	computerScoreOp.textContent = computerScore;
+}
+
+function bestOfThree(selection) {
+	let computerSelection = computerPick();
+	let youWin = isWinner(selection, computerSelection);
+	let youLose = isWinner(computerSelection, selection);
+	resultTitle.style.fontSize = "3rem";
+	newGame.style.display = "none";
+	resultBtns.style.justifyContent = "center";
+	playAgain.textContent = "next round";
+	newGame.textContent = "new game";
+	resultStyle(selection, computerSelection);
+	if (youWin) {
+		round++;
+		resultTitle.textContent = `you win round ${round}`;
+		playerScore++;
+		if (playerScore >= 2) {
+			bestOfThreeWin("player");
+		}
+	}
+	if (youLose) {
+		round++;
+		resultTitle.textContent = `house wins round ${round}`;
+		computerScore++;
+		if (computerScore >= 2) {
+			bestOfThreeWin("computer");
+		}
+	}
+	if (selection === computerSelection) {
+		resultTitle.textContent = "tie";
+	}
+	playerScoreOp.textContent = playerScore;
+	computerScoreOp.textContent = computerScore;
+	bestOfThreeCount.textContent = `${round}`;
+}
+
+function bestOfThreeWin(player) {
+	newGame.style.display = "block";
+	playAgain.style.display = "none";
+	if (player === "player") {
+		playerScoreOp.style.color = "green";
+		resultTitle.textContent = "you win";
+		resultTitle.style.color = "green";
+	}
+	if (player === "computer") {
+		computerScoreOp.style.color = "red";
+		resultTitle.textContent = "house wins";
+		resultTitle.style.color = "red";
+	}
 }
 
 function isWinner(selection, computerSelection) {
